c40f68575a2107538c18bc8c5eb51739571eb7a7
Jp Venadas <jaypheevenadas18@gmail.com>
2022-02-03 22:39:23 +0800

    Welcome Page

---
+++ script.js
@@ -0,0 +1,7 @@
+let play = document.querySelector('.play-button');
+let difOptions = document.querySelector('.difficulty-page');
+let body = document.querySelector('body');
+
+play.addEventListener('click', ()=>{
+ difOptions.style.top = '0';
+})

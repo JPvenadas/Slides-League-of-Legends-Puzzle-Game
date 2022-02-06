00f954ae282d770b6a49ab18ff240387c505c4fb
Jp Venadas <jaypheevenadas18@gmail.com>
2022-02-06 16:55:41 +0800

    Added Pictures and design structure

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let play = document.querySelector('.play-button');
-let difOptions = document.querySelector('.difficulty-page');
-let body = document.querySelector('body');
+let difficultyPage = document.querySelector('.difficulty-page');
+let welcomePage = document.querySelector('.welcome-page');
+let back = document.querySelectorAll('.back');
+let instructionpage = document.querySelector('.instruction-page');
+let instruction = document.querySelector('.instructions-button');
+let Easymode = document.querySelector('.easy')
+
+
 
 play.addEventListener('click', ()=>{
- difOptions.style.top = '0';
+    difficultyPage.style.top = '0';
+})
+back.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        difficultyPage.style.top = "-200%";
+        instructionpage.style.top = "-200%";
+    })
+});
+instruction.addEventListener('click', ()=>{
+    instructionpage.style.top = '0';
+})
+
+Easymode.addEventListener('click', ()=>{
+    difficultyPage.style.top = "-200%";
+    setTimeout(()=>{
+        welcomePage.style.top = "-200%";
+    },500)
 })

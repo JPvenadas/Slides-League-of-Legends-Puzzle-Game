3cd3796ec86bbeafa3cf3dcb2b0ee3c231ea8e24
Jp Venadas <jaypheevenadas18@gmail.com>
2022-02-07 19:45:36 +0800

    Enhance the Interface

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-let play = document.querySelector('.play-button');
-let difficultyPage = document.querySelector('.difficulty-page');
-let welcomePage = document.querySelector('.welcome-page');
-let back = document.querySelectorAll('.back');
-let instructionpage = document.querySelector('.instruction-page');
-let instruction = document.querySelector('.instructions-button');
-let Easymode = document.querySelector('.easy')
+let start = document.querySelector('.start-button')
+let options = document.querySelector('.options-container')
+let firstback = document.querySelector('.back1')
+let secondback = document.querySelector('.back2')
+let playerName = document.getElementById('name')
+let btnSave = document.querySelector('.save-name')
+let upperOptions = document.querySelector('.options-top')
+let lowerOptions = document.querySelector('.options-bot')
+let radio = document.querySelectorAll('input[type=radio]')
+let modeContainer = document.querySelectorAll('.mode-container');
 
-
-
-play.addEventListener('click', ()=>{
-    difficultyPage.style.top = '0';
+start.addEventListener('click', ()=>{
+    options.style.top = "0"
 })
-back.forEach(button =>{
-    button.addEventListener('click', ()=>{
-        difficultyPage.style.top = "-200%";
-        instructionpage.style.top = "-200%";
-    })
-});
-instruction.addEventListener('click', ()=>{
-    instructionpage.style.top = '0';
+firstback.addEventListener('click', ()=>{
+    options.style.top = "-400%"
 })
-
-Easymode.addEventListener('click', ()=>{
-    difficultyPage.style.top = "-200%";
-    setTimeout(()=>{
-        welcomePage.style.top = "-200%";
-    },500)
+playerName.addEventListener('keydown', ()=>{
+   if(playerName.value !== "" || playerName.value !== " "){
+    btnSave.innerHTML = "Done"
+   }
+})
+btnSave.addEventListener('click', ()=>{
+    upperOptions.style.transform = "translateX(-100%)";
+    lowerOptions.style.transform = "translateX(0)";
+})
+secondback.addEventListener('click', ()=>{
+    upperOptions.style.transform = "translateX(0)";
+    lowerOptions.style.transform = "translateX(-100%)";
 })

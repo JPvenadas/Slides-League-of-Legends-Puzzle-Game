999e3162da8983c9a0326b1ff44b2d5efb5711b4
Jp Venadas <jaypheevenadas18@gmail.com>
2022-02-13 10:35:02 +0800

    Implement inversion checker (to avoid unsolvable puzzle)

--- script.js
+++ script.js
@@ -67,21 +67,21 @@ easy.addEventListener('click', ()=>{
     gameLevel = "Easy"
     CreateTiles(9);
     puzzleBoard.style.gridTemplateAreas = '"P1 P2 P3" "P4 P5 P6" "P7 P8 P9"'
-    setPosition(Randomizer(9))
+    setPosition(solvablearray(9))
    
 })
 normal.addEventListener('click', ()=>{
     gameLevel = "Normal"
     CreateTiles(16)
     puzzleBoard.style.gridTemplateAreas = '"P1 P2 P3 P10" "P4 P5 P6 P11" "P7 P8 P9 P12" "P13 P14 P15 P16"'
-    setPosition(Randomizer(16))
+    setPosition(solvablearray(16))
    
 })
 difficult.addEventListener('click', ()=>{
     gameLevel = "Difficult"
     CreateTiles(25)
     puzzleBoard.style.gridTemplateAreas = '"P1 P2 P3 P10 P17" "P4 P5 P6 P11 P18" "P7 P8 P9 P12 P19" "P13 P14 P15 P16 P20" "P21 P22 P23 P24 P25"'
-    setPosition(Randomizer(25))
+    setPosition(solvablearray(25))
    
 })
 modes.forEach(mode =>{
@@ -90,7 +90,7 @@ modes.forEach(mode =>{
         unlock(unlocklist, trimGridArea(document.querySelector('.empty-tile').style.gridArea), tiles)
         playerName2.innerText = playerName.value
         goal.style.background = `url(Pictures/${gameLevel}-mode/Full-img.png) no-repeat center`
-        goalIMG.src = `/Pictures/${gameLevel}-mode/Full-img.png`
+        goalIMG.src = `Pictures/${gameLevel}-mode/Full-img.png`
         options.style.top = "-400%";
         setTimeout(() => {
         welcome.style.top = "-400%"
@@ -111,15 +111,15 @@ modes.forEach(mode =>{
 })
 restart.addEventListener('click', ()=>{
     if(gameLevel == "Easy"){
-        setPosition(Randomizer(9))
+        setPosition(solvablearray(9))
         
     }
     if(gameLevel == "Normal"){
-        setPosition(Randomizer(16))
+        setPosition(solvablearray(16))
         
     }
     if(gameLevel == "Difficult"){
-        setPosition(Randomizer(25))
+        setPosition(solvablearray(25))
     }
     let tiles = Array.from(puzzleBoard.querySelectorAll('button'))
     unlock(unlocklist, trimGridArea(document.querySelector('.empty-tile').style.gridArea), tiles)
@@ -156,7 +156,7 @@ let Randomizer = (num) =>{
            array.push(randomNum);
        }
     }
-    return array
+   return array
 }
 
 let setPosition = (Randomnumbers) =>{
@@ -203,9 +203,52 @@ let unlock = (list, emptyposition, tiles) =>{
         }
     })
 }
- 
+ let solvablearray = (num) =>{
+        let numstatus = num%2
+        let inversionsStatus
+        let random
+        do{
+            random = Randomizer(num)
+            inversionsStatus = inversionchecker(random)
+        }while(numstatus === inversionsStatus || numstatus == inversionsStatus)
+        return random
+ }
 let trimGridArea = (GridPositionstring) =>{
     let regex = /[P][0-9]+/
     let result = GridPositionstring.match(regex)
     return result[0]
 }
+
+function inversionchecker(array){
+    let inversions= 0;
+    let NormalArrange = [1,2,3,10,4,5,6,11,7,8,9,12,13,14,15,16]
+    let difficultArrange = [1,2,3,10,17,4,5,6,11,18,7,8,9,12,19,13,14,15,16,20,21,22,23,24,25]
+    for(let num1 = 0; num1< (array.length-1); num1++){
+       for(let num2 = (num1+1); num2 < (array.length-1); num2++){
+           if(array.length == 9){
+                if(array[num1]>array[num2]){
+                    inversions++
+                }
+           } 
+           if(array.length == 16){
+                if(NormalArrange.indexOf(array[num1])>(NormalArrange.indexOf(array[num2]))){
+                    inversions++
+                }
+           } 
+           if(array.length == 25){
+            if(difficultArrange.indexOf(array[num1])>(difficultArrange.indexOf(array[num2]))){
+                inversions++
+            }
+           }
+       }
+    }
+    console.log(inversions)
+    return oddOrEven(inversions)
+}
+function oddOrEven(num){
+    let remainder = num % 2 
+    return remainder
+}
+
+console.log(inversionchecker([9,1,2,3,4,5,6,7,8]))
+ 
